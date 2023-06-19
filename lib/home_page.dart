19d4c49642e7682ba672f17f847cc9928624df7f
Nina Gębalska <ninagebalska@gmail.com>
2023-06-19 22:50:47 +0200

    Add appbar and search field to Homepage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,55 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: const Color(0xFFF6FEF7),
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('Botanicare'),
+            SizedBox(width: 4),
+            Icon(Icons.spa_outlined),
+          ],
+        ),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(36),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: ListView(children: [
+          Text('Let\'s Find Your Plants!',
+              style: GoogleFonts.lato(fontSize: 28)),
+          const SizedBox(height: 16),
+          TextField(
+            controller: _searchController,
+            decoration: InputDecoration(
+              hintText: 'Search...',
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.clear),
+                onPressed: () => _searchController.clear(),
+              ),
+              prefixIcon: IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {},
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
