92ea36229b63ffa0ae185c90400e9846018f00de
Nina Gębalska <ninagebalska@gmail.com>
2023-07-03 21:33:31 +0200

    Create plant model

---
+++ lib/domain/models/plant.dart
@@ -0,0 +1,10 @@
+class Plant {
+  final int id;
+  final String name;
+  final String picture;
+  final bool poisonous;
+  final String watering;
+  final String sunlight;
+
+  Plant(this.id, this.name, this.picture, this.poisonous, this.watering, this.sunlight);
+}
